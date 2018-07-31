3a5748d47c8180136375e0c98183fa24de149f8a
Steven Stromberg <steven.stromberg@familysearch.org>
2018-07-31 11:00:05 -0600

    Updated prophandler with lots of improvements and bugfixes

--- mjs/propHandler.mjs
+++ mjs/propHandler.mjs
@@ -4,68 +4,93 @@ export function attrChangedCB(el, args) {
   const [attrName, oldVal, newVal] = args;
 
   if (oldVal !== newVal && el._propNames) {
-    el[Object.entries(el._propNames).find(([key, value]) => value === attrName)[0]] = newVal;
+    const propKey = Object.entries(el._propNames).find(([key, value]) => value === attrName)[0]; // eslint-disable-line no-unused-vars
+    const currentPropVal = typeof el[propKey] === 'object' ? JSON.stringify(el[propKey]) : el[propKey];
+
+    if (currentPropVal !== newVal && !(typeof currentPropVal === 'number' && currentPropVal === +newVal)) {
+      el[propKey] = newVal;
+    }
   }
 }
 
 export function getAttrNames(propObj) {
-  return propObj.map(prop => camelToDash(getName(prop)));
+  if (Array.isArray(propObj)) {
+    return propObj.map(prop => camelToDash(getName(prop)));
+  }
+
+  console.error('propObj should be an array of objects!');
 }
 
 export function setUpProps(el, propObj) {
-  setTimeout(() => {
-    if (el && Array.isArray(propObj)) {
-      el._propNames = {};
+  if (el && Array.isArray(propObj)) {
+    el._propNames = {};
 
-      propObj.forEach(prop => {
-        const name = getName(prop);
+    propObj.forEach(prop => { // eslint-disable-line complexity
+      const name = getName(prop);
 
-        el._propNames[name] = camelToDash(name);
+      el._propNames[name] = camelToDash(name);
 
-        const props = {
-          get: () => el[`_${name}`] === 'true' ? true : el[`_${name}`] === 'false' ? false : el[`_${name}`] || null,
-          set: val => {
-            if (val || val === false || val === 0) {
-              el[`_${name}`] = val;
-              if (!(prop.hasOwnProperty('setAttr') && prop.setAttr === false)) {
-                el.setAttribute(el._propNames[name], val);
-              }
-            } else {
-              delete el[`_${name}`];
-              el.removeAttribute(el._propNames[name]);
+      const props = {
+        get: () => {
+          let returnValue = el[`_${name}`];
+
+          if (returnValue === 'true') {
+            returnValue = true;
+          } else if (returnValue === 'false') {
+            returnValue = false;
+          }
+
+          return returnValue;
+        },
+        set: val => {
+          if (val || val === false || val === 0) {
+            el[`_${name}`] = val;
+
+            if (!(prop.hasOwnProperty('setAttr') && prop.setAttr === false)) {
+              el.setAttribute(el._propNames[name], typeof val === 'object' ? JSON.stringify(val) : val);
             }
-          }
-        };
-
-        if (typeof prop === 'object' && !Array.isArray(prop)) {
-          if (prop.hasOwnProperty('get')) {
-            props.get = prop.get;
-          }
-
-          if (prop.hasOwnProperty('set')) {
-            props.set = val => {
-              let lastVal = el[name];
-              const setPropSuccess = prop.set.call(el, val);
-
-              if (setPropSuccess) {
-                lastVal = val;
-
-                if (!(prop.hasOwnProperty('setAttr') && prop.setAttr === false)) {
-                  el.setAttribute(el._propNames[name], typeof val === 'object' ? JSON.stringify(val) : val);
-                }
-              } else if (lastVal && el.hasAttribute(el._propNames[name])) {
-                el.setAttribute(el._propNames[name], typeof lastVal === 'object' ? JSON.stringify(lastVal) : lastVal);
-              }
-            };
+          } else {
+            delete el[`_${name}`];
+            el.removeAttribute(el._propNames[name]);
           }
         }
+      };
 
-        Object.defineProperty(el, name, props);
+      if (typeof prop === 'object' && !Array.isArray(prop)) {
+        if (prop.hasOwnProperty('get')) {
+          props.get = prop.get;
+        }
 
-        el[name] = el.getAttribute(el._propNames[name]) || (prop.default || prop.default === false || prop.default === 0 ? typeof prop.default === 'object' ? JSON.stringify(prop.default) : prop.default : null);
-      });
-    }
-  });
+        if (prop.hasOwnProperty('set')) {
+          props.set = val => {
+            let lastVal = el[name];
+            const setPropSuccess = prop.set.call(el, val);
+
+            if (setPropSuccess) {
+              const newVal = el[name]; // Get value in case there are custom getters or setters
+              if (!(prop.hasOwnProperty('setAttr') && prop.setAttr === false)) {
+                el.setAttribute(el._propNames[name], typeof newVal === 'object' ? JSON.stringify(newVal) : newVal);
+              }
+            } else if (lastVal && el.hasAttribute(el._propNames[name])) {
+              el.setAttribute(el._propNames[name], typeof lastVal === 'object' ? JSON.stringify(lastVal) : lastVal);
+            }
+          };
+        }
+      }
+
+      Object.defineProperty(el, name, props);
+
+      let defaultValue;
+
+      if (prop.default || prop.default === false || prop.default === 0) {
+        defaultValue = prop.default;
+      }
+
+      el[name] = el.getAttribute(el._propNames[name]) || defaultValue;
+    });
+  } else {
+    console.error('propObj should be an array of objects!');
+  }
 }
 
 function getName(val) {
