f652f27accf4622748480ad92f43b74ccbf8eab4
GyozaGuy <gyozaguy@gmail.com>
2019-06-11 01:27:41 -0600

    Added html() tagged template function, integrated it with dom() and custom attributes

--- mjs/templateUtils.mjs
+++ mjs/templateUtils.mjs
@@ -4,20 +4,62 @@ export function css(css) {
   return styleEl;
 }
 
-export function html(htmlArr, ...strings) {
+export function dom(htmlString, context) {
   const template = document.createElement('template');
-  let htmlString = '';
+  template.innerHTML = htmlString;
+  const elements = template.content;
 
-  htmlArr.forEach((a, i) => {
-    htmlString += `${a}${strings[i] || ''}`;
+  if (context) {
+    [...elements.children].forEach(child => {
+      convertAttributes(child, context);
+    });
+  }
+
+  return elements;
+}
+
+const domFunctions = {};
+
+export function html(htmlArr, ...strings) {
+  return htmlArr.reduce((acc, cur, i) => {
+    let currentString = strings[i] || '';
+
+    if (typeof currentString === 'object') {
+      if (Array.isArray(currentString)) {
+        currentString = currentString.join('');
+      } else {
+        currentString = JSON.stringify(currentString);
+      }
+    } else if (typeof currentString === 'function') {
+      const id = Math.random().toString(36).substr(2, 9);
+
+      domFunctions[id] = currentString;
+      currentString = id;
+    }
+
+    return `${acc}${cur}${currentString}`;
+  }, '');
+}
+
+function convertAttributes(element, context) {
+  [...element.attributes].forEach(attr => {
+    if (attr.name === '@name') {
+      context[attr.value] = element;
+      element.removeAttribute(attr.name);
+    } else if (/^#/.test(attr.name)) {
+      element.addEventListener(attr.name.substr(1), domFunctions[attr.value]);
+      element.removeAttribute(attr.name);
+      delete domFunctions[attr.value];
+    }
   });
 
-  template.innerHTML = htmlString;
-
-  return template.content;
+  [...element.children].forEach(child => {
+    convertAttributes(child, context);
+  });
 }
 
 export default {
   css,
+  dom,
   html
 };
